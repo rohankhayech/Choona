6191636d8aba6ba8b608bd9b5ee1fe25b1bd04d1
Rohan Khayech <rohankhayech@gmail.com>
2025-08-15 18:10:34 +0800

    Updated dependencies.
    Signed-off-by: Rohan Khayech <rohankhayech@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.rohankhayech.choona"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.4.2"
+        versionCode = 11
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
