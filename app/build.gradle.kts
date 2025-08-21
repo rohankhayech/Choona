92117c029ca400005552cb4b720e84d4c1ca6686
Rohan Khayech <49182055+rohankhayech@users.noreply.github.com>
2025-08-21 13:05:44 +0800

    Bumped minor version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.rohankhayech.choona"
         minSdk = 24
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.4.3"
+        versionCode = 12
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
