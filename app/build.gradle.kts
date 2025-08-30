a8614ed06aca10b80249e6ca4d293f4746f5cbf4
Rohan Khayech <rohankhayech@gmail.com>
2025-08-30 14:18:58 +0800

    Bump minor version.
    Signed-off-by: Rohan Khayech <rohankhayech@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.rohankhayech.choona"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.5.0"
+        versionCode = 13
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
