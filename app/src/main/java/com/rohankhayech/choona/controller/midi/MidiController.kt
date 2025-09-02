d6fe938bc7cdfb11fe1b352412e224a67587fc92
Rohan Khayech <rohankhayech@gmail.com>
2025-09-02 23:53:10 +0800

    Refactored MIDI controller to use Kotlin and suspend functions rather than multithreading.
    Signed-off-by: Rohan Khayech <rohankhayech@gmail.com>

--- app/src/main/java/com/rohankhayech/choona/controller/midi/MidiController.kt
+++ app/src/main/java/com/rohankhayech/choona/controller/midi/MidiController.kt
@@ -15,77 +15,39 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+package com.rohankhayech.choona.controller.midi
 
-package com.rohankhayech.choona.controller.midi;
-
-import com.rohankhayech.music.Notes;
-
-import org.billthefarmer.mididriver.GeneralMidiConstants;
-import org.billthefarmer.mididriver.MidiConstants;
-import org.billthefarmer.mididriver.MidiDriver;
+import kotlin.experimental.or
+import com.rohankhayech.music.Notes
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import org.billthefarmer.mididriver.GeneralMidiConstants
+import org.billthefarmer.mididriver.MidiConstants
+import org.billthefarmer.mididriver.MidiDriver
 
 /**
  * Controller class responsible for generating and sending MIDI events representing guitar notes.
+ * @property numStrings The number of strings this controller can simultaneously play a note on.
  *
  * @author Rohan Khayech
  */
-public final class MidiController {
+class MidiController(val numStrings: Int) {
 
-    private MidiDriver midiDriver;
+    /** The system MIDI driver. */
+    private var midiDriver: MidiDriver = MidiDriver.getInstance()
 
-    /** Array of threads for each string. Used to wait for the duration of a note. */
-    private Thread[] stringThread;
+    /** Mutex for exclusive writing to the MIDI driver. */
+    private var mutex = Mutex()
 
-    /** Array of mutexes for each string thread. */
-    private Object[] stringMutex;
-
-    /**
-     * Constructs a new MIDI controller.
-     * @param numStrings The number of strings this controller can simultaneously play a note on.
-     */
-    public MidiController(int numStrings) {
-        init(numStrings);
+    /** Starts the midi controller and system driver. */
+    fun start() {
+        midiDriver.start()
     }
 
-    private void init(int numStrings) {
-        midiDriver = MidiDriver.getInstance();
-
-        // Setup string thread array.
-        stringThread = new Thread[numStrings];
-        stringMutex = new Object[numStrings];
-
-        for (int i=0; i<numStrings; i++) {
-            // Setup string thread.
-            stringThread[i] = null;
-            stringMutex[i] = new Object();
-        }
-    }
-
-    /**
-     * Starts the midi controller and system driver.
-     */
-    public void start() {
-        midiDriver.start();
-    }
-
-    /**
-     * Stops all playing notes and then stops the midi controller and system driver.
-     */
-    public void stop() {
-        for (int i=0; i<stringThread.length; i++) {
-            stopNote(i);
-        }
-        midiDriver.stop();
-    }
-
-    /**
-     * Plays the specified note on the specified string for the specified duration.
-     * @param string The position of the string on the tuning.
-     * @param midiNote The MIDI note number to play.
-     * @param duration The duration of the note, in ms.
-     */
-    public void playNote(int string, int midiNote, long duration) {
-        playNote(string, midiNote, duration, GeneralMidiConstants.ELECTRIC_GUITAR_CLEAN);
+    /** Stops all playing notes and then stops the midi controller and system driver. */
+    fun stop() {
+        midiDriver.stop()
     }
 
     /**
@@ -95,66 +57,48 @@ public final class MidiController {
      * @param duration The duration of the note, in ms.
      * @param instrument The MIDI instrument code to play.
      */
-    public void playNote(int string, int midiNote, long duration, byte instrument) {
-        stopNote(string);
-
+    suspend fun playNote(string: Int, midiNote: Int, duration: Long, instrument: Byte = GeneralMidiConstants.ELECTRIC_GUITAR_CLEAN) {
         // Set channel
-        final byte channel = (byte)(string < 9 ? string : string+1); // Channel 9 is reserved for percussion, skip it.
+        val channel = getChannel(string)
 
         // Play note.
-        synchronized (stringMutex[string]) {
-            stringThread[string] = new Thread(() -> {
                 try {
-                    setInstrument(channel, instrument);
+                    mutex.withLock {
+                        setInstrument(channel, instrument)
 
-                    // Send note on event
-                    byte[] event = new byte[3];
-                    event[0] = (byte) (MidiConstants.NOTE_ON | channel); // Status byte and channel
-                    event[1] = (byte) midiNote; // Pitch (midi note number)
-                    event[2] = (byte) 0x7F; // Velocity
-                    midiDriver.write(event);
+                        // Send note on event
+                        val event = ByteArray(3)
+                        event[0] = MidiConstants.NOTE_ON or channel // Status byte and channel
+                        event[1] = midiNote.toByte() // Pitch (midi note number)
+                        event[2] = 0x7F.toByte() // Velocity
+                        midiDriver.write(event)
+                    }
 
                     // Wait for duration of note.
-                    Thread.sleep(duration);
-                } catch (InterruptedException e) {
-                    // Cancel thread sleep.
+                    delay(duration)
                 } finally {
-                    synchronized (stringMutex[string]) {
-                        // Stop the note playing after duration or interrupted.
-                        // Send note off event.
-                        byte[] event = new byte[3];
-                        event[0] = (byte) (MidiConstants.NOTE_OFF | channel); // Status byte and channel
-                        event[1] = (byte) midiNote; // Pitch (midi note number)
-                        event[2] = (byte) 0x00; // Velocity
-                        midiDriver.write(event);
-
-                        // Clean up thread.
-                        stringThread[string] = null;
-                    }
+                    stopNote(string, midiNote)
                 }
-            }, "string_thread_" + string);
-
-            stringThread[string].start();
-        }
-    }
+            }
 
     /**
      * Stops the currently playing note on the specified string.
      * @param string The position of the string on the tuning.
      */
-    public void stopNote(int string) {
-        if (isNotePlaying(string)) {
-            stringThread[string].interrupt();
-        }
-    }
+    suspend fun stopNote(string: Int, midiNote: Int) {
+        // Set channel
+        val channel = getChannel(string)
 
-    /**
-     * Converts the specified note index to a MIDI note number.
-     * @param noteIndex The internal note index.
-     * @return The corresponding MIDI note number.
-     */
-    public static int noteIndexToMidi(int noteIndex) {
-        return noteIndex+Notes.A4_MIDI_NOTE_NUMBER;
+        // Stop the note playing after duration or interrupted.
+        // Send note off event.
+        val event = ByteArray(3)
+        event[0] = MidiConstants.NOTE_OFF or channel // Status byte and channel
+        event[1] = midiNote.toByte() // Pitch (midi note number)
+        event[2] = 0x00.toByte() // Velocity
+
+        mutex.withLock {
+            midiDriver.write(event)
+        }
     }
 
     /**
@@ -162,14 +106,28 @@ public final class MidiController {
      * @param channel The MIDI channel number.
      * @param instrument The MIDI instrument code to set.
      */
-    private void setInstrument(int channel, byte instrument) {
-        byte[] event = new byte[2];
-        event[0] = (byte) (MidiConstants.PROGRAM_CHANGE | (byte)channel);
-        event[1] = instrument;
-        midiDriver.write(event);
+    private fun setInstrument(channel: Byte, instrument: Byte) {
+        val event = ByteArray(2)
+        event[0] = MidiConstants.PROGRAM_CHANGE or channel
+        event[1] = instrument
+        midiDriver.write(event)
     }
 
-    private boolean isNotePlaying(int string) {
-        return stringThread[string] != null;
+    /**
+     * Gets the MIDI channel number for the string.
+     */
+    private fun getChannel(string: Int): Byte {
+        return (if (string < 9) string else string + 1).toByte() // Channel 9 is reserved for percussion, skip it.
+    }
+
+    companion object {
+        /**
+         * Converts the specified note index to a MIDI note number.
+         * @param noteIndex The internal note index.
+         * @return The corresponding MIDI note number.
+         */
+        fun noteIndexToMidi(noteIndex: Int): Int {
+            return noteIndex + Notes.A4_MIDI_NOTE_NUMBER
+        }
     }
 }
--- app/src/main/java/com/rohankhayech/choona/view/activity/TunerActivity.kt
+++ app/src/main/java/com/rohankhayech/choona/view/activity/TunerActivity.kt
@@ -353,33 +353,39 @@ class TunerActivity : ComponentActivity() {
 
     /** Plays the string selection sound for the specified [string]. */
     private fun playStringSelectSound(string: Int) {
-        midi.playNote(
-            string,
-            MidiController.noteIndexToMidi(vm.tuner.tuning.value.getString(string).rootNoteIndex),
-            150,
-            vm.tuner.tuning.value.instrument.midiInstrument
-        )
+        lifecycleScope.launch {
+            midi.playNote(
+                string,
+                MidiController.noteIndexToMidi(vm.tuner.tuning.value.getString(string).rootNoteIndex),
+                150,
+                vm.tuner.tuning.value.instrument.midiInstrument
+            )
+        }
     }
 
     /** Plays the note selection sound for the specified [noteIndex]. */
     private fun playNoteSelectSound(noteIndex: Int) {
-        midi.playNote(
-            0,
-            MidiController.noteIndexToMidi(noteIndex),
-            150,
-            Instrument.GUITAR.midiInstrument
-        )
+        lifecycleScope.launch {
+            midi.playNote(
+                0,
+                MidiController.noteIndexToMidi(noteIndex),
+                150,
+                Instrument.GUITAR.midiInstrument
+            )
+        }
     }
 
     /** Plays the in tune sound for the selected string. */
     private fun playInTuneSound() {
         val string = vm.tuner.selectedString.value
-        midi.playNote(
-            string,
-            MidiController.noteIndexToMidi(vm.tuner.tuning.value.getString(string).rootNoteIndex) + 12,
-            50,
-            GeneralMidiConstants.MARIMBA
-        )
+        lifecycleScope.launch {
+            midi.playNote(
+                string,
+                MidiController.noteIndexToMidi(vm.tuner.tuning.value.getString(string).rootNoteIndex) + 12,
+                50,
+                GeneralMidiConstants.MARIMBA
+            )
+        }
     }
 
     /**
