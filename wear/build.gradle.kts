f8e5066b60a7c981f5a99d3f30324489fb39aedd
Rohan Khayech <rohankhayech@gmail.com>
2026-01-05 00:54:24 +0800

    Update target sdk for wear. Signed-off-by: Rohan Khayech <rohankhayech@gmail.com>
    Signed-off-by: Rohan Khayech <rohankhayech@gmail.com>

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     defaultConfig {
         applicationId = "com.rohankhayech.choona"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 2
         versionName = "1.6.0-wear-alpha.1"
     }
