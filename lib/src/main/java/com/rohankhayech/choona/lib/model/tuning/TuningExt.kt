6e09af2bd22c1558cde312a5ab57383f34137313
Rohan Khayech <rohankhayech@gmail.com>
2026-04-11 15:30:49 +0800

    Fixed unit test for hasChanges.
    Signed-off-by: Rohan Khayech <rohankhayech@gmail.com>

--- lib/src/main/java/com/rohankhayech/choona/lib/model/tuning/TuningExt.kt
+++ lib/src/main/java/com/rohankhayech/choona/lib/model/tuning/TuningExt.kt
@@ -23,6 +23,4 @@ package com.rohankhayech.choona.lib.model.tuning
  * @param other The tuning to check equivalence with.
  * @return True if the other tuning has the same strings as this tuning, false otherwise.
  */
-infix fun Tuning.equivalentTo(other: Tuning?): Boolean {
-    return this.equivalentTo(other)
-}
+infix fun Tuning.equivalentTo(other: Tuning?): Boolean = equivalentTo(other)
